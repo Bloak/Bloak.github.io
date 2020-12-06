ec4c546ed3687c304503f1c636d44fda984348cf
Bloak <55962111+Bloak@users.noreply.github.com>
2020-12-06 18:28:40 -0500

    .

--- hello-ml5/sketch.js
+++ hello-ml5/sketch.js
@@ -6,7 +6,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/dog.jpeg');
+  img = loadImage('images/bird.jpg');
 }
 
 function setup() {
