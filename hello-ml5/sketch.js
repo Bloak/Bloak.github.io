140f191b0b7c793028a0c618a6b51c6ff6fe10d1
Bloak <55962111+Bloak@users.noreply.github.com>
2020-12-06 18:22:43 -0500

    Update sketch.js

--- hello-ml5/sketch.js
+++ hello-ml5/sketch.js
@@ -12,7 +12,7 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   classifier.classify(img, gotResult);
-  image(img, 0, 0, 400, 400);
+  image(img, 0, 0, width, height);
 }
 
 // A function to run when we get any errors and the results
