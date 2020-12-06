311803e78cea30f4ed9346c5f82a3891f2e4cf11
Bloak <55962111+Bloak@users.noreply.github.com>
2020-12-06 18:13:47 -0500

    Update sketch.js

--- hello-ml5/sketch.js
+++ hello-ml5/sketch.js
@@ -6,7 +6,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/bird.jpg');
+  img = loadImage('../images/bird.jpg');
 }
 
 function setup() {
