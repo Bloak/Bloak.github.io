36d1b704860488f3fda6e34713881bc752d70c61
Bloak <55962111+Bloak@users.noreply.github.com>
2023-01-25 18:01:26 -0500

    /

--- touhou_piano_library/arrangement_info.js
+++ touhou_piano_library/arrangement_info.js
@@ -1,4 +1,4 @@
-function get_entry_name() {
+function get_entry_id() {
     var id = GetURLParameter("id");
     if (id) {
         return id;
@@ -20,33 +20,209 @@ async function get_entry_info(id) {
     }
 }
 
+async function set_title(data) {
+    var title = "File Not Found";
+
+    var [title_og, title_en] = await get_arrangement_title(data);
+    if (title_og !== null) {
+        title = title_og;
+        if (title_en !== null && translation_enabled()) {
+            title = title_en;
+        }
+    }
+
+    // update h1
+    var title_elem = document.getElementById("title");
+    title_elem.innerText = title;
+
+    // update html title
+    document.title = title;
+}
+
+function fill_plain_metadata(data) {
+    var entries = ["circle", "album", "type", "difficulty"];
+    for (var entry of entries) {
+        var elem = document.getElementById(entry);
+        if (entry in data) {
+            var content = data[entry];
+            if (Array.isArray(content)) {
+                content = content.join(" & ");
+            }
+            elem.innerText = content;
+        }
+        else {
+            elem.parentNode.remove();
+        }
+    }
+}
+
+async function fill_person_metadata(data) {
+    var entries = ["arranger", "transcriber"];
+    // Both entries can be a list of multiple persons
+    for (var entry of entries) {
+        var elem = document.getElementById(entry);
+        if (entry in data) {
+            var content = data[entry];
+            if (Array.isArray(content)) {
+                // it is a list of names
+                var p = await create_person_links(content);
+                elem.appendChild(p);
+
+                // change entry tag to plural
+                var tag = elem.previousElementSibling;
+                tag.innerText += "s";
+            }
+            else {
+                var name = content;
+                var blob = await create_person_link(name);
+                elem.appendChild(blob);
+            }
+        }
+        else {
+            elem.parentNode.remove();
+        }
+    }
+}
+
+async function fill_theme_links(data) {
+    var elem = document.getElementById("original-theme");
+
+    // get all the themes
+    var themes = [data.original];
+    if ("original_ex" in data) {
+        themes = themes.concat(data.original_ex);
+    }
+
+    for (var id of themes) {
+        var blob = await create_theme_link(id);
+        elem.appendChild(blob);
+
+        // to separate each blob
+        var new_line = document.createElement("br");
+        elem.appendChild(new_line);
+    }
+    elem.removeChild(elem.lastChild);
+
+    // change entry tag to plural
+    if (themes.length > 1) {
+        var tag = elem.previousElementSibling;
+        tag.innerText += "s";
+    }
+}
+
 function generate_sheet_music_link(id, data) {
     var url = "";
     var text = "";
     if (data.pdf === "default") {
         text = id + ".pdf";
-        url = "../arrange/" + text;
+        url = "https://bloak.github.io/arrange/" + text;
     }
     else {
         text = "Found Here";
         url = data.pdf;
     }
 
-    var a = document.createElement("a");
-    var atext = document.createTextNode(text);
-    a.setAttribute("href", url);
-    a.appendChild(atext);
+    var a = create_link(url, text, true);
 
     var sheet_music_link = document.getElementById("sheet-music-link");
     sheet_music_link.appendChild(a);
+
+    if (data.pdf === "default") {
+        generate_sheet_music_preview(url);
+    }
+}
+
+async function generate_sheet_music_preview(url) {
+    // Loaded via <script> tag, create shortcut to access PDF.js exports.
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+    // The workerSrc property shall be specified.
+    pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+    // Asynchronous download of PDF
+    var loadingTask = pdfjsLib.getDocument(url);
+    loadingTask.promise.then(function(pdf) {
+        console.log('PDF loaded');
+  
+        // Fetch the first page
+        var pageNumber = 1;
+        pdf.getPage(pageNumber).then(function(page) {
+            var canvas = document.getElementById('pdf-preview');
+    
+            var desiredWidth = 500;
+            desiredWidth = Math.min(desiredWidth, canvas.parentElement.clientWidth);
+            var viewport = page.getViewport({ scale: 1, });
+            var scale = desiredWidth / viewport.width;
+            viewport = page.getViewport({ scale: scale, });
+
+            // Prepare canvas using PDF page dimensions
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.promise.then(function () {
+                console.log('Page rendered');
+            });
+        });
+    }, function (reason) {
+        // PDF loading error
+        console.error(reason);
+    });
+}
+
+async function show_recordings(data) {
+    var table = document.getElementById("recordings");
+
+    for (var entry of data.performance) {
+        var new_row = table.insertRow();
+
+        var cell_1 = new_row.insertCell();
+        var name = entry.performer;
+        if (name !== null) {
+            var blob = await create_person_link(name);
+            cell_1.appendChild(blob);
+        }
+
+        var cell_2 = new_row.insertCell();3
+        var link = create_url_link(entry.link, true);
+        cell_2.appendChild(link);
+    }
+}
+
+function add_comment(data) {
+    var p = document.getElementById("comment");
+
+    if (!("comment" in data)) {
+        p.parentNode.remove();
+        return;
+    }
+
+    var text = data.comment;
+    text = text_to_html(text);
+    p.innerHTML = text;
 }
 
 async function main() {
-    var id = get_entry_name();
+    var id = get_entry_id();
     var data = await get_entry_info(id);
-    // console.log(data);
+
+    set_title(data);
+
+    fill_plain_metadata(data);
+    fill_person_metadata(data);
+    fill_theme_links(data);
 
     generate_sheet_music_link(id, data);
+
+    show_recordings(data);
+
+    add_comment(data);
 }
 
 document.onload = main();
---
+++ touhou_piano_library/arranger_info.js
@@ -0,0 +1,116 @@
+function get_entry_name() {
+    var id = GetURLParameter("name");
+    if (id) {
+        return id;
+    }
+    else {
+        console.error("No name received.");
+        return null;
+    }
+}
+
+async function get_entry_info(id) {
+    var data = await get_json("arrangers.json");
+    if (id in data) {
+        return data[id];
+    }
+    else {
+        console.error("Entry does not exist.");
+        return null;
+    }
+}
+
+function set_title(name, data) {
+    var title = name;
+    if ("alt_name" in data && translation_enabled()) {
+        title = data.alt_name;
+    }
+
+    // update h1
+    var title_elem = document.getElementById("title");
+    title_elem.innerText = title;
+
+    // update html title
+    document.title = title;
+}
+
+function get_arrangements(name, all_arrangements) {
+    // get all the (ids of) arrangements that belong to the arranger
+    var results = [];
+    for (var a_id in all_arrangements) {
+        var arrangement = all_arrangements[a_id];
+        if (arrangement.arranger === name || (Array.isArray(arrangement.arranger) && arrangement.arranger.includes(name))) {
+            results.push(a_id);
+        }
+    }
+    // sort by ascending difficulty
+    /* results.sort((a, b) => {
+        var d_a = all_arrangements[a].difficulty;
+        var d_b = all_arrangements[b].difficulty;
+        if (d_b === d_a) return 0;
+        if (d_b === "?") return -1;
+        if (d_b > d_a) return -1;
+        return 1;
+    }); */
+    
+    return results;
+}
+
+async function display_arrangements(ids, all_arrangements) {
+    for (var id of ids) {
+        var arrangement = all_arrangements[id];
+
+        var title = "/";
+        if (arrangement.title !== null) {
+            title = arrangement.title;
+            if ("title_en" in arrangement && translation_enabled()) {
+                title = arrangement.title_en;
+            }
+        }
+
+        // only the main theme
+        var theme_link = await create_theme_link(arrangement.original);
+
+        var type = arrangement.type;
+
+        var difficulty = arrangement.difficulty;
+
+        var link = create_link("./arrangement_info.html?id=" + id, "View");
+
+        var table = document.getElementById("table");
+        var new_row = table.insertRow();
+        var cell1 = new_row.insertCell(); cell1.innerText = title;
+        var cell2 = new_row.insertCell(); cell2.appendChild(theme_link);
+        var cell3 = new_row.insertCell(); cell3.innerText = type;
+        var cell4 = new_row.insertCell(); cell4.innerText = difficulty;
+        var cell5 = new_row.insertCell(); cell5.appendChild(link);
+    }
+}
+
+function add_comment(data) {
+    var p = document.getElementById("comment");
+
+    if (!("comment" in data)) {
+        p.parentNode.remove();
+        return;
+    }
+
+    var text = data.comment;
+    text = text_to_html(text);
+    p.innerHTML = text;
+}
+
+async function main() {
+    var name = get_entry_name();
+    var data = await get_entry_info(name);
+
+    set_title(name, data);
+
+    var all_arrangements = await get_json("touhou_arranges.json");
+    var current_theme_arrangement_ids = get_arrangements(name, all_arrangements);
+    display_arrangements(current_theme_arrangement_ids, all_arrangements);
+
+    add_comment(data);
+}
+
+document.onload = main();
