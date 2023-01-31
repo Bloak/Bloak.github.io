985f288dcd7e1a1af94d48311dc559029853abdf
Bloak <55962111+Bloak@users.noreply.github.com>
2023-01-31 18:12:06 -0500

    /

--- touhou_piano_library/touhou_work_list.js
+++ touhou_piano_library/touhou_work_list.js
@@ -15,7 +15,61 @@ function activate_collapsibles() {
     }
 }
 
-function main() {
+function display_themes(touhou_themes, arrangements) {
+    for (var theme_id in touhou_themes) {
+        var theme_info = touhou_themes[theme_id];
+        var work_id = theme_info.collection;
+        var div = document.getElementById(work_id);
+        var table = div.children[1];
+        var tbody = table.children[2];
+        var new_row = tbody.insertRow();
+
+        var theme_title = theme_info.title;
+        if (translation_enabled() && "title_en" in theme_info) {
+            theme_title = theme_info.title_en;
+        }
+
+        var arrangement_count = 0;
+        for (var a_id in arrangements) {
+            var arrangement = arrangements[a_id];
+            if (arrangement.original === theme_id) {
+                arrangement_count++;
+            }
+        }
+
+        var cell_1 = new_row.insertCell(); cell_1.innerHTML = theme_id;
+        var cell_2 = new_row.insertCell(); cell_2.appendChild(create_link("./touhou_theme_info.html?id="+theme_id, theme_title));
+        var cell_3 = new_row.insertCell(); cell_3.innerHTML = arrangement_count;
+    }
+}
+
+function instantiate_collapsibles(touhou_works) {
+    var template = document.getElementById("template");
+    for (var id in touhou_works) {
+        var new_collapsible = template.cloneNode(true);
+
+        new_collapsible.setAttribute("id", id);
+        new_collapsible.style.display = "block";
+
+        var button = new_collapsible.children[0];
+        var work_info = touhou_works[id];
+        var work_title = (!translation_enabled()) ? work_info.title : work_info.title_en;
+        button.innerHTML = work_title;
+
+        var parent = document.getElementById(work_info.type);
+        parent.appendChild(new_collapsible);
+    }
+}
+
+async function main() {
+    var touhou_works = await get_json("touhou_works.json");
+    var touhou_themes = await get_json("touhou_original_themes.json");
+    var arrangements = await get_json("touhou_arranges.json");
+
+    instantiate_collapsibles(touhou_works);
+
+    display_themes(touhou_themes, arrangements);
+
     activate_collapsibles();
 }
 
