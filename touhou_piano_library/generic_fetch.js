ea8867fdb4aec6762b3dbec2e0b39331b1f82560
Bloak <55962111+Bloak@users.noreply.github.com>
2023-02-01 18:35:43 -0500

    /

--- touhou_piano_library/generic_fetch.js
+++ touhou_piano_library/generic_fetch.js
@@ -212,3 +212,28 @@ function resize_table_font(table_id) {
         table.style.fontSize = (Math.floor(font_size_ratio * 100)).toString() + "%";
     }
 }
+
+function remove_hover() {
+    function hasTouch() {
+        return 'ontouchstart' in document.documentElement
+            || navigator.maxTouchPoints > 0
+            || navigator.msMaxTouchPoints > 0;
+    }
+      
+    if (hasTouch()) { // remove all the :hover stylesheets
+        try { // prevent exception on browsers not supporting DOM styleSheets properly
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+      
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+      
+                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                        styleSheet.deleteRule(ri);
+                    }
+                }
+            }
+        } catch (ex) {}
+    }
+}
